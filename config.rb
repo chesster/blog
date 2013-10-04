09e491418e869ca336627704b6c57eb211c51998
Lukasz Malik <lukasz.a.malik@gmail.com>
2013-10-04 18:37:43 +0200

    disquis + article tempalte

--- config.rb
+++ config.rb
@@ -4,105 +4,46 @@ Time.zone = "UTC"
 
 activate :i18n
 activate :livereload
-
-# Deploy
-#activate :deploy do |deploy|
-#end
-
+activate :automatic_image_sizes
 activate :syntax, :line_numbers => true
 
-# Blog settings
+
+set :css_dir, 'stylesheets'
+set :images_dir, 'images'
+set :js_dir, 'javascripts'
+
+set :helpers_dir, "helpers"
+set :layout, :_auto_layout
+set :layouts_dir, "layouts"
+set :locales_dir, "locales"
+
+ # Blog settings
 activate :blog do |blog|
   blog.sources = "articles/:year-:month-:day-:title.html"
   blog.default_extension = ".markdown"
 
-  blog.permalink = ":year-:month-:day-:title.html"
+  blog.permalink = ":title-:year:month:day.html"
   blog.year_link = "archive/:year.html"
   blog.month_link = "archive/:year/:month.html"
   blog.day_link = "archive/:year/:month/:day.html"
 
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
+  blog.layout = "layouts/article_layout.haml"
 
   # blog.prefix = "blog"
   blog.taglink = ":tag.html"
-  blog.layout = "layout"
-  
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-
   blog.paginate = false
-  # blog.per_page = 10
-  # blog.page_link = "page/:num"
 end
 
 page "/feed.xml", :layout => false
 
-###
-# Compass
-###
-
-# Susy grids in Compass
-# First: gem install susy
-# require 'susy'
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
-
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-
-# Automatic image dimensions on image_tag helper
-activate :automatic_image_sizes
-
-
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
-
 # Build-specific configuration
 configure :build do
-
   activate :minify_html
   activate :gzip
   activate :minify_css
   activate :minify_javascript
   activate :cache_buster
   activate :relative_assets
-
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
 end
